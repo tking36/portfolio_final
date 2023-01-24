a806a38d51846fd20816ba9721f387547f1eac5b
Taylor King <taylorking@taylors-mbp.lan>
2023-01-24 18:57:23 -0500

    Media Queries and changing projects page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import pdfFile from '../myresume.pdf'
 
 
@@ -9,6 +9,7 @@ const Nav = (props) => {
     const [about, setAbout] = useState(false)
     const [experience, setExperience] = useState(false)
     const [resume, setResume] = useState(false)
+    const [projects, setProjects] = useState(false)
 
     const showButtons = () => {
         setShow(!show)
@@ -20,6 +21,7 @@ const Nav = (props) => {
         setAbout(false)
         setExperience(false)
         setResume(false)
+        setProjects(false)
     }
 
     const showAbout = () => {
@@ -27,6 +29,7 @@ const Nav = (props) => {
         setAbout(true)
         setExperience(false)
         setResume(false)
+        setProjects(false)
     }
 
     const showExperience = () => {
@@ -34,6 +37,7 @@ const Nav = (props) => {
         setAbout(false)
         setExperience(true)
         setResume(false)
+        setProjects(false)
     }
 
     const showResume = () => {
@@ -41,6 +45,15 @@ const Nav = (props) => {
         setAbout(false)
         setExperience(false)
         setResume(true)
+        setProjects(false)
+    }
+
+    const showProjects = () => {
+        setHome(false)
+        setAbout(false)
+        setExperience(false)
+        setResume(false)
+        setProjects(true)
     }
 
 
@@ -55,41 +68,46 @@ const Nav = (props) => {
             showExperience()
         } else if (num == 3) {
             showResume()
+        } else if (num == 4) {
+            showProjects()
         }
       }
+
+      const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        const handleResize = () => setWindowWidth(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
 
     
 
 
   return (
-    <div className='navigation'>
+    <div className= {windowWidth > 500 ? 'nav-desktop' : 'navigation'} > 
 
         
         <div className='nav-buttons'>
-            {show ? 
-            <>
-            <button title='Home'  className={ home ? "home-button navB btn btn-outline-success glow active" : "home-button navB btn btn-outline-success glow"} onClick={() => showPage(0)} ><i class="bi bi-house-fill"></i></button>
-            <button title="About Me" className={ about ? "about-button navB btn btn-outline-success glow active" : "about-button navB btn btn-outline-success glow"} onClick={() => showPage(1)}><i class="bi bi-person-fill"></i></button>
-            </>
-            : <a title="Git Hub" href='https://github.com/tking36' target='blank'><i class="bi bi-github glow link-me"></i></a>} 
+            
+            <button title='Home'  className={ home ? "home-button navB btn btn-outline-success glow active" : "home-button navB btn btn-outline-success glow"} onClick={() => showPage(0)} > {windowWidth > 500? <h1 className='navD'>Home</h1> : <i class="bi bi-house-fill"></i> }</button>
 
+            <button title="About Me" className={ about ? "about-button navB btn btn-outline-success glow active" : "about-button navB btn btn-outline-success glow"} onClick={() => showPage(1)}> {windowWidth > 500? <h1 className='navD'>About</h1> : <i class="bi bi-person-fill"></i> } </button>
             
-            {show ? 
+            <button title="Experience" className={ experience ? "skills-button navB btn btn-outline-success glow active" : "skills-button navB btn btn-outline-success glow"} onClick={() => showPage(2)}> {windowWidth > 500? <h1 className='navD'>Skills</h1> : <i class="fa-solid fa-user-graduate"></i> } </button>
+
+            <button title="Menu" className= {projects ?  'about-button navB btn btn-outline-success glow active' : 'about-button navB btn btn-outline-success glow' } onClick={() => showPage(4)}>{windowWidth > 500? <h1 className='navD'>Projects</h1> : <i class="bi bi-hammer"></i> } </button>
             
-            <button title="links" className='about-button navB btn btn-outline-success glow' onClick={showButtons}><i class="bi bi-link"></i></button>
-            : <button title="Menu" className='about-button navB btn btn-outline-success glow' onClick={showButtons}><i class="bi bi-list"></i></button>
-}
+            <a title="Resume" className={ resume ? "about-button navB btn btn-outline-success glow active" : "about-button navB btn btn-outline-success glow"} href={pdfFile} target='blank'> {windowWidth > 500? <h1 className='navD'>Resume</h1> : <i class="bi bi-file-earmark-arrow-down"></i> } </a>
             
-            {show ? 
-            <>
-            <button title="Experience" className={ experience ? "skills-button navB btn btn-outline-success glow active" : "skills-button navB btn btn-outline-success glow"} onClick={() => showPage(2)}><i class="fa-solid fa-user-graduate"></i></button>
-            <a title="Resume" className={ resume ? "about-button navB btn btn-outline-success glow active" : "about-button navB btn btn-outline-success glow"} href={pdfFile} target='blank'><i class="bi bi-file-earmark-arrow-down"></i></a>
-            </>
-            : <a title="Linked In" href='https://github.com/tking36' target='blank'><i class="bi bi-linkedin glow link-me"></i></a>}
         </div>
     </div> 
   )
 }
 
 export default Nav
+
+{/* <a title="Gßit Hub" href='https://github.com/tking36' target='blank'><i class="bi bi-github glow link-me"></i></a> */}
+{/* <a title="Linked In" href='https://github.com/tking36' target='blank'><i class="bi bi-linkedin glow link-me"></i></a> */}
+{/* <button title="links" className='about-button navB btn btn-outline-success glow' onClick={showButtons}><i class="bi bi-link"></i></button> */}
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,11 +6,50 @@ const Projects = (props) => {
         props.setPage(num)
       }
 
+      const Music = require ('../images/music.gif')
+      const Space = require ('../images/space.gif')
+
   return (
     <>
-    <div id="carouselExampleCaptions" class="carousel slide">
-       
-        <div class="carousel-inner">
+    <div id="projects">
+        <div id="first" className="card" >
+            <img src={Music} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Music Library</h5>
+                <p className="card-text">This is a react app that uses JavaScript and JSX for storing and displaying your favorite music, including properties such as song name, artist, album, genre and year.</p>
+                <li class="list-group-item"><a className='link' href='https://my-music-library321.herokuapp.com/' target='blank'>Demo</a></li>
+                <li class="list-group-item "><a className='link' href='https://github.com/tking36/back-end' target='blank'>GIT HUB Back-End</a></li>
+                <li class="list-group-item"><a className='link' href='https://github.com/tking36/front-end' target='blank'>GIT HUB Front-End</a></li>
+            </div>
+        </div>
+
+        <div className="card" >
+            <img src={Space} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Music Library</h5>
+                <p className="card-text">This is a react app that uses JavaScript and JSX for storing and displaying your favorite music, including properties such as song name, artist, album, genre and year.</p>
+                <li class="list-group-item"><a className='link' href='https://space-invaders.herokuapp.com/' target='blank'>Demo</a></li>
+                <br></br>
+                <li class="list-group-item"><a className='link' href=' https://github.com/tking36/adventure_front_end' target='blank'>GIT HUB Front-End</a></li>
+            </div>
+        </div>
+
+        <div className="card" >
+            <img src={Space} className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">Music Library</h5>
+                <p className="card-text">This is a react app that uses JavaScript and JSX for storing and displaying your favorite music, including properties such as song name, artist, album, genre and year.</p>
+                <li class="list-group-item"><a className='link' href='https://space-invaders.herokuapp.com/' target='blank'>Demo</a></li>
+                <br></br>
+                <li class="list-group-item"><a className='link' href=' https://github.com/tking36/adventure_front_end' target='blank'>GIT HUB Front-End</a></li>
+            </div>
+        </div>
+
+    </div>
+
+    
+    
+        {/* <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="https://i.imgur.com/563bF15.png" class="d-block w-100" alt="music"/>
                 <div class="carousel-caption  d-md-block">
@@ -55,11 +94,7 @@ const Projects = (props) => {
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="visually-hidden">Next</span>
-        </button>
-    </div>
-
-    
-    
+        </button> */}
     </>
 )
 }
